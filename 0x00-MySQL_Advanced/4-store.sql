3c70d293d06e3b41d839e82518e6e4aea6e96603
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-10-19 21:41:15 +0300

    solved

--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,9 +1,10 @@
 -- Decrease the quantity of an item after adding a new order
 
-CREATE TRIGGER decrement
-AFTER INSERT ON orders
-FOR EACH ROW
-BEGIN
-    UPDATE items 
-    SET quantity = quantity - NEW.order_quantity 
-    WHERE item_id = NEW.item_id;
+CREATE TRIGGER decrement AFTER
+INSERT
+    ON orders FOR EACH ROW
+UPDATE items
+SET
+    quantity = quantity - NEW.order_quantity
+WHERE
+    item_id = NEW.item_id;
