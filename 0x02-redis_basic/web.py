13eaa91ce57555c40fac0154de7cec5a85075f59
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-10-24 02:02:03 +0300

    web.py

---
+++ 0x02-redis_basic/web.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python3
+"""Import required modules"""
+import redis
+import requests
+from functools import wraps
+from typing import Callable
+
+redis_instance = redis.Redis()
+
+
+def cache_page(func: Callable) -> Callable:
+    """Decorator to cache the result of a function in Redis."""
+
+    @wraps(func)
+    def wrapper(url: str) -> str:
+        """Wrapper to cache page content and count URL access."""
+        cache_key = f"cache:{url}"
+        count_key = f"count:{url}"
+
+        cached_page = redis_instance.get(cache_key)
+        if cached_page:
+            return cached_page.decode("utf-8")
+
+        redis_instance.incr(count_key)
+        page_content = func(url)
+        redis_instance.setex(cache_key, 10, page_content)
+
+        return page_content
+
+    return wrapper
+
+
+@cache_page
+def get_page(url: str) -> str:
+    """Fetches the HTML content of a URL and returns it."""
+    response = requests.get(url)
+    return response.text
