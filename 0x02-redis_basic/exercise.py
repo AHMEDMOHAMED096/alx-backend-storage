f15f7221d1f577085feae1bf144f4ae6568d8588
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-10-24 01:38:04 +0300

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -2,7 +2,7 @@
 """Import required modules"""
 import redis
 from uuid import uuid4
-from typing import Union
+from typing import Union, Callable
 
 
 class Cache:
@@ -18,3 +18,20 @@ class Cache:
         key = str(uuid4())
         self._redis.set(key, data)
         return key
+
+    def get(
+        self,
+        key: str,
+        fn: Callable = None,
+    ) -> Union[str, bytes, int, float]:
+        """Retrieves a value from Redis storage."""
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
+
+    def get_str(self, key: str) -> Union[str, bytes]:
+        """Method to retrieve a value as a string"""
+        return self.get(key, lambda x: x.decode("utf-8"))
+
+    def get_int(self, key: str) -> int:
+        """Method to retrieve a value as an integer"""
+        return self.get(key, lambda x: int(x))
