868e7faac5d72719d812bf5f8ea7566f4bf39c97
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-10-24 01:43:20 +0300

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,6 +3,7 @@
 import redis
 from uuid import uuid4
 from typing import Union, Callable
+import functools
 
 
 class Cache:
@@ -12,12 +13,11 @@ class Cache:
         """Initializes a Cache instance"""
         self._redis = redis.Redis()
         self._redis.flushdb()
+        self._counts = {}
 
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Method to store data with certain key"""
-        key = str(uuid4())
-        self._redis.set(key, data)
-        return key
+        return super().store(data)
 
     def get(
         self,
@@ -25,8 +25,7 @@ class Cache:
         fn: Callable = None,
     ) -> Union[str, bytes, int, float]:
         """Retrieves a value from Redis storage."""
-        data = self._redis.get(key)
-        return fn(data) if fn is not None else data
+        return super().get(key, fn)
 
     def get_str(self, key: str) -> Union[str, bytes]:
         """Method to retrieve a value as a string"""
@@ -35,3 +34,26 @@ class Cache:
     def get_int(self, key: str) -> int:
         """Method to retrieve a value as an integer"""
         return self.get(key, lambda x: int(x))
+
+
+def count_calls(func: Callable) -> Callable:
+    """Decorator that increments the call count for a method."""
+
+    @functools.wraps(func)
+    def wrapper(self, *args, **kwargs):
+        key = f"{self.__class__.__name__}.{func.__qualname__}"
+
+        if key not in self._counts:
+            self._counts[key] = 0
+
+        old_value = func(self, *args, **kwargs)
+        new_count = self._counts[key] + 1
+        self._counts[key] = new_count
+
+        return old_value
+
+    wrapper.__doc__ = f"Counting calls for {func.__qualname__}"
+    return wrapper
+
+
+Cache.store = count_calls(Cache.store)
