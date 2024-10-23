4cd0395dc7d2fbd6df88fc9ff3c73bec9cc7e79a
ahmed mohamed <ahmed.mohamed91196@gmail.com>
2024-10-24 01:46:55 +0300

    exercise.py

--- 0x02-redis_basic/exercise.py
+++ 0x02-redis_basic/exercise.py
@@ -3,7 +3,20 @@
 import redis
 from uuid import uuid4
 from typing import Union, Callable
-import functools
+from functools import wraps
+
+
+def count_calls(method: Callable) -> Callable:
+    """Decorator to count how many times a method is called."""
+
+    @wraps(method)
+    def wrapper(self, *args, **kwargs):
+        """Method to count calls and execute the original method."""
+        key = method.__qualname__
+        self._redis.incr(key)
+        return method(self, *args, **kwargs)
+
+    return wrapper
 
 
 class Cache:
@@ -13,11 +26,13 @@ class Cache:
         """Initializes a Cache instance"""
         self._redis = redis.Redis()
         self._redis.flushdb()
-        self._counts = {}
 
+    @count_calls
     def store(self, data: Union[str, bytes, int, float]) -> str:
         """Method to store data with certain key"""
-        return super().store(data)
+        key = str(uuid4())
+        self._redis.set(key, data)
+        return key
 
     def get(
         self,
@@ -25,7 +40,8 @@ class Cache:
         fn: Callable = None,
     ) -> Union[str, bytes, int, float]:
         """Retrieves a value from Redis storage."""
-        return super().get(key, fn)
+        data = self._redis.get(key)
+        return fn(data) if fn is not None else data
 
     def get_str(self, key: str) -> Union[str, bytes]:
         """Method to retrieve a value as a string"""
@@ -34,26 +50,3 @@ class Cache:
     def get_int(self, key: str) -> int:
         """Method to retrieve a value as an integer"""
         return self.get(key, lambda x: int(x))
-
-
-def count_calls(func: Callable) -> Callable:
-    """Decorator that increments the call count for a method."""
-
-    @functools.wraps(func)
-    def wrapper(self, *args, **kwargs):
-        key = f"{self.__class__.__name__}.{func.__qualname__}"
-
-        if key not in self._counts:
-            self._counts[key] = 0
-
-        old_value = func(self, *args, **kwargs)
-        new_count = self._counts[key] + 1
-        self._counts[key] = new_count
-
-        return old_value
-
-    wrapper.__doc__ = f"Counting calls for {func.__qualname__}"
-    return wrapper
-
-
-Cache.store = count_calls(Cache.store)
